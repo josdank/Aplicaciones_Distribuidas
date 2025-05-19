03329e230171bbf4ad56ef539b19abd855433736
Safronjos <151254944+josdank@users.noreply.github.com>
2025-05-19 15:15:08 -0500

    Delete cliente directory

--- cliente/src/cliente/test/test.java
+++
@@ -1,9 +0,0 @@
-package cliente.test;
-
-import cliente.clase.OperacionesVista;
-
-public class test {
-    public static void main(String[] args) {
-        new OperacionesVista(); // Inicia la interfaz gráfica
-    }
-}
